95c6080a2550604ba4cd8b938444cd8b00a46425
Kenneth Chen <kenchen0606@gmail.com>
2021-11-26 00:20:31 +0000

    get reservations from lightbnb db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -77,7 +77,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  //return getAllProperties(null, 3);
+  return pool.query(`
+    SELECT properties.*, reservations.*, avg(rating) AS average_rating 
+    FROM  reservations 
+    JOIN properties ON properties.id = property_id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    HAVING now()::date > reservations.start_date
+    LIMIT $2;`,
+    [guest_id, limit])
+      .then((result) => result.rows)
+      .catch((err) => err.message);
 }
 exports.getAllReservations = getAllReservations;
 
